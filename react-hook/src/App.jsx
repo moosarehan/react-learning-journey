cf7195354a32b0570353e06aa52a7b4c103baf65
moosarehan <moosarehan4@gmail.com>
2026-07-06 14:28:41 +0500

    feat: implement main App layout and navigation (App.jsx)

---
+++ react-hook/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { StateExample } from "./hooks/use-state/index";
+
+function App() {
+  return (
+    <>
+      <StateExample />
+    </>
+  );
+}
+
+export default App;
